c9367789181ed1b91542a437cbcdb773a125470d
RodrigoAar <ajm22279@uvg.edu.gt>
2025-04-03 12:10:20 -0600

    Update Product.java
    Método constructor y toString para la clase producto

--- Hoja de trabajo 7/src/main/java/org/example/Product.java
+++ Hoja de trabajo 7/src/main/java/org/example/Product.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Product {
+    String sku;
+    double priceRetail;
+    double priceCurrent;
+    String productName;
+    String category;
+
+    public Product(String sku, double priceRetail, double priceCurrent, String productName, String category) {
+        this.sku = sku;
+        this.priceRetail = priceRetail;
+        this.priceCurrent = priceCurrent;
+        this.productName = productName;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "SKU: " + sku + ", Price Retail: " + priceRetail + ", Price Current: " + priceCurrent + ", Name: " + productName + ", Category: " + category;
+    }
+    
 }
